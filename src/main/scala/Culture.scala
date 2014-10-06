adc05b56a3589b6917f85c08d4895c89ae4e18b6
hibikir <hibikir@gmail.com>
2014-10-05 21:01:45 -0500

    Added Slack to Culture, making the test pass

--- src/main/scala/Culture.scala
+++ src/main/scala/Culture.scala
@@ -1,4 +1,6 @@
-package teamo;
+package teamo
 
-class Culture {
+//Slack: ratio of time between time spent on getting the basic work done and total time expenditure
+//So with a slack of 2, we use double the minimum time needed to get a working feature
+case class Culture(slack:Double = 0.0) {
 }
--- src/test/scala/teamo/teamo_test.scala
+++ src/test/scala/teamo/teamo_test.scala
@@ -9,10 +9,10 @@ import scala.concurrent.duration._
 class SomeSpec extends FunSuite with GeneratorDrivenPropertyChecks with Matchers {
 
   def simulate(t: Team, p: Project, c: Culture, d: FiniteDuration) = {
-   Results(3)
+   Results(3*c.slack)
   }
 
-  def moreSlack(culture: Culture) = culture
+  def moreSlack(culture: Culture) = culture.copy(slack = culture.slack + 0.05)
 
   test("after a while, the team with more slack is more productive") {
     forAll (TeamGen(),
@@ -20,9 +20,8 @@ class SomeSpec extends FunSuite with GeneratorDrivenPropertyChecks with Matchers
       Gen.const(new Culture))
       { (team : Team, project: Project, culture: Culture) =>
       val aWhile = 90.days
-      val results = simulate(team, project, culture, aWhile);
+      val results = simulate(team, project, culture, aWhile)
       val resultsWithMoreSlack = simulate(team, project, moreSlack(culture), aWhile)
-
       resultsWithMoreSlack.productValue should be > results.productValue
     }
   }
