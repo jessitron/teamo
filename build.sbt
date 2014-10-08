4ac852a83c5a63f0044af0620be9dee2c1f96972
Jessica Kerr <jessitron@gmail.com>
2014-10-07 21:26:23 -0500

    add akka dependency

--- build.sbt
+++ build.sbt
@@ -4,8 +4,8 @@ version := "0.1"
 
 scalaVersion := "2.11.2"
 
-
 libraryDependencies ++= Seq(
+  "com.typesafe.akka" %% "akka-actor" % "2.3.6" % "compile",
   "org.scalacheck" %% "scalacheck" % "1.11.6" % "compile",
   "org.scalatest" % "scalatest_2.11" % "2.2.1" % "test"
   )
--- src/main/scala/teamo/Simulation.scala
+++ src/main/scala/teamo/Simulation.scala
@@ -1,13 +1,14 @@
 package teamo;
 
 import scala.concurrent.duration._
+import akka.actor._
 
 object Simulation {
 
   def run(t: Team, p: Project, c: Culture, d: FiniteDuration) = {
+
+    val system = ActorSystem("teamo")
+    system.shutdown();
     Results (3 * c.slack)
   }
-
-
-
 }
