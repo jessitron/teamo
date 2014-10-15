3b9e3a9e9c4972389c6347cfb45912f52a3f574f
hibikir <hibikir@gmail.com>
2014-10-14 22:23:30 -0500

    Use teamNatureGen

--- src/test/scala/teamo/teamo_test.scala
+++ src/test/scala/teamo/teamo_test.scala
@@ -10,6 +10,7 @@ class SomeSpec extends FunSuite with GeneratorDrivenPropertyChecks with Matchers
    Simulation.run(t,d)
   }
 
+  def moreSlack(team:TeamNature):TeamNature = team.copy(culture = moreSlack(team.culture))
   def moreSlack(culture: Culture) = culture.copy(slack = culture.slack + 0.05)
 
   test("after a while, the team with more slack is more productive") {
@@ -17,8 +18,8 @@ class SomeSpec extends FunSuite with GeneratorDrivenPropertyChecks with Matchers
       CultureGen())
       { (team : TeamNature, culture: Culture) =>
       val aWhile = 90.days
-      val results = simulate(team, culture, aWhile)
-      val resultsWithMoreSlack = simulate(team, moreSlack(culture), aWhile)
+      val results = simulate(team, aWhile)
+      val resultsWithMoreSlack = simulate(moreSlack(team), aWhile)
       resultsWithMoreSlack.productValue should be > results.productValue
     }
   }
